e3af8b600981c1d9c3a544842a502f90c8d14098
mutuajoseph <wambuajoseph756@gmail.com>
2024-08-28 10:59:26 +0300

    python basic fundamentals

---
+++ main.py
@@ -0,0 +1,70 @@
+# print("Hello World!")
+
+# VARIABLE (temporary container that gives habitat to values)
+# DATA TYPES (classification that provides the nature of values)
+
+# COMMON DATA TYPES
+# 1. String (str) - text
+name = "John Doe"
+# 2. Integer (int) - whole numbers
+age = 23
+# 3. Float (float) - decimal numbers
+weight = 65.5
+# 4. Boolean (bool) - True or False
+is_student = True
+
+# print(name, age, weight, is_student)
+
+# BASIC OPERATIONS AND INPUT 
+# Arithmetic Operators
+# + (Addition),-(subtraction), *(multiplication), /(division), %(modulus), **(exponentiation) // ()
+
+num_one = input("Enter first number: ")
+num_two = input("Enter second number: ")
+
+result = int(num_one) + int(num_two)
+result_two = int(num_one) - int(num_two)
+result_three = int(num_one) * int(num_two)
+result_four = int(num_one) / int(num_two)
+result_five = int(num_one) % int(num_two)
+result_six = int(num_one) ** int(num_two)
+result_seven = int(num_one) // int(num_two)
+print(result, result_two, result_three, result_four, result_five, result_six, result_seven)
+
+# Assignment Operators - Assign a value to a variable (+=, -=, *=, /=, %=, **=, //=)
+# = Assignment Operator
+num_one = 10
+
+# += Addition Assignment Operator
+num_one += 5 # num_one = num_one + 5
+print(num_one)
+
+# -= Subtraction Assignment Operator
+num_one -= 5 # num_one = num_one - 5
+print(num_one)
+
+# *= Multiplication Assignment Operator
+num_one *= 5 # num_one = num_one * 5
+print(num_one)
+
+# /= Division Assignment Operator
+num_one /= 2 # num_one = num_one / 5
+print(num_one)
+
+# %= Modulus Assignment Operator
+num_one %= 2 # num_one = num_one % 5
+print(num_one)
+
+# **= Exponentiation Assignment Operator
+num_one **= 2 # num_one = num_one ** 5
+print(num_one)
+
+# //= Floor Division Assignment Operator
+num_one = -5
+num_one //= 2 # num_one = num_one // 5
+print(num_one)
+
+
+
+# Comparison Operators
+# Logical Operators
