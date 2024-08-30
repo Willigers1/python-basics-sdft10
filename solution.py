98f1f3974265bb4ae47bcfdcf7636e37397ec1fd
mutuajoseph <wambuajoseph756@gmail.com>
2024-08-30 10:07:42 +0300

    Added the BDD of the challenge

---
+++ solution.py
@@ -0,0 +1,15 @@
+#BDD
+# 1. Feature - calculate the number of ways to pair boys and girls 
+
+#Scenario: Pairing 2 boys and 2 girls from a group of  5 boys and girls 
+# given an array [5,5,4] where  5 represents the number of boys 
+# and 5 represents the number of girls and 4 represents the number of people to pair
+# 
+# When I run the MatchinCouples function 
+# 
+# Then the function should return 200 because there are 10 waus to choose 2 boys from 5 , 
+# 10 ways to to choose 2 girls from 5 and 2 ways to pair the selected boys and girls  
+
+#pseudo code 
+
+#solution
